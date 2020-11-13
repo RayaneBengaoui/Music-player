ba6c27fd2e9f5aec3c7b50db4b8f34d818e9f1ca
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-11-13 12:46:42 +0100

    Creating components + Setting up fontawesome library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+//Adding components
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
