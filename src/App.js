114595a844e941581a72c3006cac2ea5b3a588a6
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-11-16 10:32:17 +0100

    Adding initial state

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import data from "./util";
 //Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+import { useState } from "react";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
