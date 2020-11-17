3e235f446c3407ee76b9c4da1f640245a338686c
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-11-17 12:08:43 +0100

    Adding song selection through the library

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
-      <Library songs={songs} />
+      <Library songs={songs} setCurrentSong={setCurrentSong} />
     </div>
   );
 }
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,19 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
-          <LibrarySong song={song} />
+          <LibrarySong
+            setCurrentSong={setCurrentSong}
+            songs={songs}
+            song={song}
+            id={song.id}
+            key={song.id}
+          />
         ))}
       </div>
     </div>
