a08ff9404658d5743d5e8a7bafa612cdf8f92ce6
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-11-18 13:58:04 +0100

    Adding mobile view + transition effects

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
       <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} isPlaying={isPlaying} />
       <Player
